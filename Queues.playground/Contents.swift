66676c92c003263e89340bc614cd1d7ca373cca2
Christopher Hicks <visrule@protonmail.com>
2022-05-15 14:33:54 -0500

    recursion

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,3 +1,47 @@
 import Foundation
 
-var greeting = "Hello, playground"
+struct Queue<T> {
+    
+    var array: [T] = []
+    init() { }
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    // Gives item at top of queue
+    var peek: T? {
+        return array.first
+    }
+    
+    // Add item to queue
+    mutating func enqueue(_ element: T) -> Bool {
+        array.append(element)
+        return true
+    }
+    
+    // Remove from queue
+    mutating func dequeue() -> T? {
+        return isEmpty ? nil : array.removeFirst()
+    }
+    
+}
+
+extension Queue: CustomStringConvertible {
+    var description: String {
+        return String(describing: array)
+    }
+}
+
+
+var queue = Queue<Int>()
+queue.enqueue(10)
+queue.enqueue(2)
+queue.enqueue(34)
+queue.enqueue(55)
+
+print(queue)
+
+queue.dequeue()
+
+print(queue)
--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//MARK: Recursion/ Recursive - the process in which a function calls itself directly or indirectly
+
+// Base case - ends recursion
+// Recursive case - calls recursively
+
+// less lines of code
+
+
+func factorial(number: Int) -> Int {
+    // base case
+    if number == 0 {
+        return 1
+    }
+    // recursive case
+    return number * factorial(number: number - 1)
+}
+
+
+//let result = factorial(number: 5)
+//print(result)
+
+
+//MARK: Power
+//2^3 = 2 * 2 * 2 = 8 (2 to the power of 3)
+
+func power(number: Int, pow: Int) -> Int {
+    // base case
+    if pow == 0 {
+        return 1
+    }
+    // recursive case
+    return number * power(number: number, pow: pow - 1)
+}
+
+let result = power(number: 2, pow: 3)
+print(result)
